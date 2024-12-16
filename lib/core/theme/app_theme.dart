59ed3231536e557b7c229d218c5d6f5076781186
Nyi Nyi Soe <nyinyisoe.dev@gmail.com>
2024-12-17 02:42:57 +0700

    feat/added login page

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,36 +4,68 @@ import 'package:shopit/core/theme/app_color.dart';
 class AppTheme {
   final lightTheme = ThemeData(
     textTheme: const TextTheme(
-      displayLarge:
-          TextStyle(color: AppColor.lightPrimaryTextColor, fontSize: 32.0),
-      displayMedium:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 28.0),
-      displaySmall:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 24.0),
-      bodyLarge:
-          TextStyle(color: AppColor.lightPrimaryTextColor, fontSize: 20.0),
-      bodyMedium:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 18.0),
-      bodySmall:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 16.0),
-      labelLarge:
-          TextStyle(color: AppColor.lightPrimaryTextColor, fontSize: 14.0),
-      labelMedium:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 12.0),
-      labelSmall:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 10.0),
-      titleLarge:
-          TextStyle(color: AppColor.lightPrimaryTextColor, fontSize: 22.0),
-      titleMedium:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 20.0),
-      titleSmall:
-          TextStyle(color: AppColor.lightSecondaryTextColor, fontSize: 18.0),
+      displayLarge: TextStyle(
+          color: AppColor.lightPrimaryTextColor,
+          fontSize: 32.0,
+          fontWeight: FontWeight.bold),
+      displayMedium: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 28.0,
+          fontWeight: FontWeight.w600),
+      displaySmall: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w500),
+      bodyLarge: TextStyle(
+          color: AppColor.lightPrimaryTextColor,
+          fontSize: 20.0,
+          fontWeight: FontWeight.normal),
+      bodyMedium: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 18.0,
+          fontWeight: FontWeight.normal),
+      bodySmall: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 16.0,
+          fontWeight: FontWeight.normal),
+      labelLarge: TextStyle(
+          color: AppColor.lightPrimaryTextColor,
+          fontSize: 14.0,
+          fontWeight: FontWeight.w600),
+      labelMedium: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 12.0,
+          fontWeight: FontWeight.w500),
+      labelSmall: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 10.0,
+          fontWeight: FontWeight.w400),
+      titleLarge: TextStyle(
+          color: AppColor.lightPrimaryTextColor,
+          fontSize: 22.0,
+          fontWeight: FontWeight.bold),
+      titleMedium: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w600),
+      titleSmall: TextStyle(
+          color: AppColor.lightSecondaryTextColor,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w500),
     ),
     primaryColor: AppColor.lightPrimaryTextColor,
     iconTheme: const IconThemeData(color: AppColor.lightPrimaryTextColor),
     iconButtonTheme: IconButtonThemeData(
         style: ButtonStyle(
-            iconColor: WidgetStateProperty.all(AppColor.lightPrimaryTextColor))),
+            iconColor:
+                WidgetStateProperty.all(AppColor.lightPrimaryTextColor))),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: WidgetStateProperty.all(AppColor.lightPrimaryTextColor),
+            textStyle: WidgetStateProperty.all(
+                const TextStyle(color: AppColor.lightPrimaryTextColor)),
+            backgroundColor:
+                WidgetStateProperty.all(AppColor.lightAppBarBackgroundColor))),
     scaffoldBackgroundColor: AppColor.lightScaffoldBackgroundColor,
     appBarTheme: const AppBarTheme(
         backgroundColor: AppColor.lightAppBarBackgroundColor,
@@ -42,38 +74,68 @@ class AppTheme {
 
   final darkTheme = ThemeData(
     textTheme: const TextTheme(
-      displayLarge:
-          TextStyle(color: AppColor.darkPrimaryTextColor, fontSize: 32.0),
-      displayMedium:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 28.0),
-      displaySmall:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 24.0),
-      bodyLarge:
-          TextStyle(color: AppColor.darkPrimaryTextColor, fontSize: 20.0),
-      bodyMedium:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 18.0),
-      bodySmall:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 16.0),
-      labelLarge:
-          TextStyle(color: AppColor.darkPrimaryTextColor, fontSize: 14.0),
-      labelMedium:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 12.0),
-      labelSmall:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 10.0),
-      titleLarge:
-          TextStyle(color: AppColor.darkPrimaryTextColor, fontSize: 22.0),
-      titleMedium:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 20.0),
-      titleSmall:
-          TextStyle(color: AppColor.darkSecondaryTextColor, fontSize: 18.0),
+      displayLarge: TextStyle(
+          color: AppColor.darkPrimaryTextColor,
+          fontSize: 32.0,
+          fontWeight: FontWeight.bold),
+      displayMedium: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 28.0,
+          fontWeight: FontWeight.w600),
+      displaySmall: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w500),
+      bodyLarge: TextStyle(
+          color: AppColor.darkPrimaryTextColor,
+          fontSize: 20.0,
+          fontWeight: FontWeight.normal),
+      bodyMedium: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 18.0,
+          fontWeight: FontWeight.normal),
+      bodySmall: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 16.0,
+          fontWeight: FontWeight.normal),
+      labelLarge: TextStyle(
+          color: AppColor.darkPrimaryTextColor,
+          fontSize: 14.0,
+          fontWeight: FontWeight.w600),
+      labelMedium: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 12.0,
+          fontWeight: FontWeight.w500),
+      labelSmall: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 10.0,
+          fontWeight: FontWeight.w400),
+      titleLarge: TextStyle(
+          color: AppColor.darkPrimaryTextColor,
+          fontSize: 22.0,
+          fontWeight: FontWeight.bold),
+      titleMedium: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w600),
+      titleSmall: TextStyle(
+          color: AppColor.darkSecondaryTextColor,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w500),
     ),
     primaryColor: AppColor.darkPrimaryTextColor,
     iconTheme: const IconThemeData(color: AppColor.darkPrimaryTextColor),
     iconButtonTheme: IconButtonThemeData(
         style: ButtonStyle(
-            iconColor:
-                WidgetStateProperty.all(AppColor.darkPrimaryTextColor))),
+            iconColor: WidgetStateProperty.all(AppColor.darkPrimaryTextColor))),
     scaffoldBackgroundColor: AppColor.darkScaffoldBackgroundColor,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: WidgetStateProperty.all(AppColor.darkPrimaryTextColor),
+            textStyle: WidgetStateProperty.all(
+                const TextStyle(fontSize: 20)),
+            backgroundColor:
+                WidgetStateProperty.all(AppColor.darkAppBarBackgroundColor))),
     appBarTheme: const AppBarTheme(
       backgroundColor: AppColor.darkAppBarBackgroundColor,
     ),
