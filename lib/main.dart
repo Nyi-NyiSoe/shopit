5377cdfeb387dd354d127b67152419707e9ebe67
Nyi Nyi Soe <nyinyisoe.dev@gmail.com>
2024-12-17 03:12:51 +0700

    fix/minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopit/presentation/bloc/theme_bloc/theme_cubit.dart';
-import 'package:shopit/presentation/pages/home_page.dart';
+import 'package:shopit/presentation/pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Shop It',
             theme: theme,
-            home: const HomePage(),
+            home: const LoginPage(),
           );
         },
       ),
--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -5,50 +5,57 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/shopping.png',
-                height: 150,
-                width: 150,
-              ),
-              Text(
-                'Shop It',
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Email',
-              prefixIcon: Icon(Icons.email),
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/shopping.png',
+                      height: 150,
+                      width: 150,
+                    ),
+                    Text(
+                      'Shop It',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child:  Text('Login'),
+                ),
+              ],
             ),
           ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Password',
-              prefixIcon: Icon(Icons.lock),
-            ),
-            obscureText: true,
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child:  Text('Login'),
-          ),
-        ],
-      ),
-    ));
+        ),
+      )),
+    );
   }
 }
